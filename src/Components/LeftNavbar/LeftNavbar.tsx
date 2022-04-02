315ab77b63932d0adb80d85c81afc0288f195762
BananaKun <yanxun951224@gmail.com>
2022-04-02 13:19:59 +0800

    left navbar prototype

--- src/Components/LeftNavbar/LeftNavbar.tsx
+++ src/Components/LeftNavbar/LeftNavbar.tsx
@@ -1,53 +1,66 @@
 import "./leftNavbar.scss";
+import { GrSpotify, GrHomeRounded, GrFormAdd } from "react-icons/gr";
+import { FiSearch } from "react-icons/fi";
+import { BiBookmark } from "react-icons/bi";
+import { FcLikePlaceholder } from "react-icons/fc";
+import { CgArrowDownO } from "react-icons/cg";
 
 const LeftNavbar = () => {
   return (
-    <div className="navbar-left">
-      <div className="navbar-left-contaier">
-        <div className="container logo">
-          <i className="fab fa-spotify"></i>
-          <span className="spotify-font-logo">
+    <div className="lnb-container">
+      <div className="lnb-sec-container">
+        <div className="spotify-logo-container">
+          <GrSpotify className="spotify-logo" />
+          <span className="spotify-logo-font">
             Spotify<span className="trademark">&reg;</span>
           </span>
         </div>
-        <ul className="container nav-col">
-          <li className="container">
-            <i className="bi bi-house-door"></i>
-            <span className="col-font">Home</span>
+        <ul className="lnb-ul">
+          <a href="" className="lnb-li-a">
+            <li className="lnb-li">
+              <GrHomeRounded className="lnb-li-icon" />
+              <span className="lnb-li-font">Home</span>
+            </li>
+          </a>
+          <a href="" className="lnb-li-a">
+            <li className="lnb-li">
+              <FiSearch className="lnb-li-icon" />
+              <span className="lnb-li-font">Search</span>
+            </li>
+          </a>
+          <a href="" className="lnb-li-a">
+            <li className="lnb-li">
+              <BiBookmark className="lnb-li-icon" />
+              <span className="lnb-li-font">Your Library</span>
+            </li>
+          </a>
+        </ul>
+        <ul className="lnb-ul">
+          <li className="lnb-li">
+            <GrFormAdd className="lnb-li-icon" />
+            <span className="lnb-li-font">Create Playlist</span>
           </li>
-          <li className="container">
-            <i className="bi bi-search"></i>
-            <span className="col-font">Search</span>
-          </li>
-          <li className="container">
-            <i className="bi bi-list"></i>
-            <span className="col-font">Your Library</span>
+          <li className="lnb-li">
+            <FcLikePlaceholder className="lnb-li-icon" />
+            <span className="lnb-li-font">Liked Songs</span>
           </li>
         </ul>
-        <div className="container nav-col second-nav-col">
-          <div className="container">
-            <i className="bi bi-plus-square-fill"></i>
-            <span className="col-font">Create Playlist</span>
-          </div>
-          <div className="container">
-            <i className="bi bi-heart"></i>
-            <span className="col-font">Liked Songs</span>
-          </div>
+        <div className="break-line">
+          <div className="break-line-border"></div>
         </div>
-        <div className="container">
-          <div className="container under-border"></div>
-        </div>
-        <div className="container nav-playlist">
-          <div className="container playlist-item">
-            <span className="nav-playlist-font">Legends Never Die</span>
+        <ul className="lnb-playlist-ul">
+          <li className="lnb-li">
+            <span className="lnb-li-playlist-font">Legends Never Die</span>
+          </li>
+          <li className="lnb-li">
+            <span className="lnb-li-playlist-font">Justice</span>
+          </li>
+        </ul>
+        <div className="install-app-container">
+          <div className="install-app-in-container">
+            <CgArrowDownO className="install-app-icon" />
+            <span>Install App</span>
           </div>
-          <div className="container playlist-item">
-            <span className="nav-playlist-font">Justice</span>
-          </div>
-        </div>
-        <div className="install-app">
-          <i className="bi bi-arrow-down-circle"></i>
-          <span>Install App</span>
         </div>
       </div>
     </div>
